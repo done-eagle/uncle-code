ec886b1f1dc005541de88a137cd3dfc1370666a8
done-eagle <vlad180317@gmail.com>
2025-04-11 00:41:15 +0600

    Initial

---
+++ UncleCode.Analyzer/UncleCode.Analyzer.Sample/Examples.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System;
+using Microsoft.CSharp;
+using Microsoft.SqlServer;
+using Serilog.Context;
+using Newtonsoft.Json;
+
+namespace UncleCode.Analyzer.Sample;
+
+public class Examples
+{
+    public class MyCompanyClass
+    {
+    }
+
+    public void ToStars()
+    {
+        var spaceship = new Spaceship();
+        spaceship.SetSpeed(300000000);
+        spaceship.SetSpeed(42);
+    }
+}
